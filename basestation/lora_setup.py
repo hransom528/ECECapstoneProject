55f4a315eac5ff7de014eb4522182d7eca0721db
Ayaan Qayyum <96453808+qayyumayaan@users.noreply.github.com>
2025-03-25 15:14:16 -0400

    Minor renaming in basestation lora code

--- basestation/lora_setup.py
+++ basestation/lora_setup.py
@@ -8,14 +8,14 @@ RADIO_FREQ_MHZ = 915.0
 BAUD_RATE = 19200
 
 def get_lora_radio():
-    """Initialize and return the LoRa radio transceiver."""
-    # Setup SPI and LoRa pins (adjust these as necessary for your wiring)
+    """Initialize and return the Basestation's LoRa radio transceiver."""
+    # Setup SPI and LoRa pins
     spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-    cs = digitalio.DigitalInOut(board.RFM_CS)    # Chip select pin for LoRa radio
+    chip_select = digitalio.DigitalInOut(board.RFM_CS)    # Chip select pin for LoRa radio
     reset = digitalio.DigitalInOut(board.RFM_RST)   # Reset pin for LoRa radio
     
     # Create and configure the LoRa radio object
-    rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, RADIO_FREQ_MHZ, baudrate=BAUD_RATE)
+    rfm9x = adafruit_rfm9x.RFM9x(spi, chip_select, reset, RADIO_FREQ_MHZ, baudrate=BAUD_RATE)
     rfm9x.tx_power = 13
     print("LoRa transceiver is initialized.")
     return rfm9x
