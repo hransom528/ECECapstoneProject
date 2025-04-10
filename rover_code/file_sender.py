89a40341d9ee69c695d508d8f663221566055ac3
Omar Hamoudeh <158100580+omarHamoudeh@users.noreply.github.com>
2025-04-10 12:19:14 -0400

    adjust sleep time

--- rover_code/file_sender.py
+++ rover_code/file_sender.py
@@ -15,7 +15,7 @@ def send_file(hex_data, handler):
         packet = hex_data[start:end]
         print(packet.encode('ascii'))
         handler.rfm9x.send(packet.encode('ascii'))
-        time.sleep(0.5)
+        time.sleep(2.0)
 
     return True
 
