bfa0b1a782bb3b94f831cfeba39ccadb8191af60
Ayaan Qayyum <96453808+qayyumayaan@users.noreply.github.com>
2025-04-22 11:49:09 -0400

    Fix bug in sending

--- rover_code/command_handler.py
+++ rover_code/command_handler.py
@@ -453,7 +453,7 @@ class WiFiScanCommand(Command):
         with open('handshk-01.csv', newline='') as csvfile:
             reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
             for row in reader:
-                handler.send_response(f"→ {row}", handler.rfm9x)
+                handler.send_response(row)
         handler.send_final_token()
 
         # Cleanup scan files afterwards
